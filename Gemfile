4b54ae48a006ed4a408d3e778c9cb59a82fd57b7
Carl Thuringer <carl@groupon.com>
2012-05-15 18:35:54 -0500

    First commit!

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.3'
+gem 'pg'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'therubyracer', :platform => :ruby
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'autotest'
+  gem 'capybara'
+end
+
+# To use debugger
+# gem 'ruby-debug19', :require => 'ruby-debug'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goodaudiblereads::Application.config.secret_token = '36e1b091ce1e869b8df4d1b904ec358deac5650ae19bbb42e8cea5e1f3e99e5ee6a3813351fb2dfb0b688ee395a28fce3529a22929c6320fbbf7c78f611531eb'
